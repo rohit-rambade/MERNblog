5c044aa4e1ff0be8f02fb325a067fd189b7383b9
Rohit Rambade <rohitrambade18@gmail.com>
2023-04-07 11:19:55 +0530

    createPost style

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,26 +1,26 @@
 import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css';
-import {useState} from "react";
-import {Navigate} from "react-router-dom";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import Editor from "../Editor";
 
 export default function CreatePost() {
-  const [title,setTitle] = useState('');
-  const [summary,setSummary] = useState('');
-  const [content,setContent] = useState('');
-  const [files, setFiles] = useState('');
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
   const [redirect, setRedirect] = useState(false);
   async function createNewPost(ev) {
     const data = new FormData();
-    data.set('title', title);
-    data.set('summary', summary);
-    data.set('content', content);
-    data.set('file', files[0]);
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/post', {
-      method: 'POST',
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
       body: data,
-      credentials: 'include',
+      credentials: "include",
     });
     if (response.ok) {
       setRedirect(true);
@@ -28,22 +28,34 @@ export default function CreatePost() {
   }
 
   if (redirect) {
-    return <Navigate to={'/'} />
+    return <Navigate to={"/"} />;
   }
   return (
-    <form onSubmit={createNewPost}>
-      <input type="title"
-             placeholder={'Title'}
-             value={title}
-             onChange={ev => setTitle(ev.target.value)} />
-      <input type="summary"
-             placeholder={'Summary'}
-             value={summary}
-             onChange={ev => setSummary(ev.target.value)} />
-      <input type="file"
-             onChange={ev => setFiles(ev.target.files)} />
-      <Editor value={content} onChange={setContent} />
-      <button style={{marginTop:'5px'}}>Create post</button>
-    </form>
+    <div className="container border  flex justify-center flex-col mt-10 font-Poppins sm:w-3/4 lg:w-1/2">
+      <form className="flex flex-col gap-y-4 mt-2" onSubmit={createNewPost}>
+        <input
+          className="p-2"
+          type="title"
+          placeholder={"Title"}
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+        />
+        <input
+          className="p-2"
+          type="summary"
+          placeholder={"Summary"}
+          value={summary}
+          onChange={(ev) => setSummary(ev.target.value)}
+        />
+        <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
+        <Editor value={content} onChange={setContent} />
+        <button
+          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600  dark:hover:bg-gray-700"
+          style={{ marginTop: "5px" }}
+        >
+          Create post
+        </button>
+      </form>
+    </div>
   );
 }
