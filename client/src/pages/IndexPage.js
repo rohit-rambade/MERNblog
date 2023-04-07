6ba3016462a7f53dc307e4ccae7167e230e37ca2
Rohit Rambade <rohitrambade18@gmail.com>
2023-04-07 14:49:45 +0530

    no post status

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,6 +1,6 @@
 import Post from "../Post";
 import { useEffect, useState } from "react";
-
+import emoji from "../images/sad.png";
 export default function IndexPage() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
@@ -12,12 +12,18 @@ export default function IndexPage() {
   }, []);
   return (
     <div className="flex flex-col px-6 bg-white  ">
-      {posts.length > 0 &&
+      {posts.length > 0 ? (
         posts.map((post) => (
-          <div className="w-full">
-            <Post key={post.id} {...post} />
+          <div className="w-full" key={post.id}>
+            <Post {...post} />
           </div>
-        ))}
+        ))
+      ) : (
+        <div class="flex flex-col justify-center items-center h-screen">
+          <img class="mx-auto w-14" src={emoji} alt="" />
+          <h1 class="font-Poppins text-3xl">No Post Yet</h1>
+        </div>
+      )}
     </div>
   );
 }
